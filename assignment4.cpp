a9fc67718591d5e89126c28efe633b3b73bda793
rohithvan1 <rohithvna1@gmail.com>
2025-10-17 11:51:22 +0530

    Update assignment4.cpp

--- assignment4.cpp
+++ assignment4.cpp
@@ -3,45 +3,44 @@
 //Answer
 
 #include <iostream>
-#include <cmath> // for M_PI (π)
 using namespace std;
 
 inline float areaCircle(float radius) {
-return M_PI * radius * radius;
+return 3.14 * radius * radius;
 }
 
 inline float areaSquare(float side) {
-   return side * side;
+     return side * side;
 }
 
 inline float areaRectangle(float length, float width) {
-   return length * width;
+     return length * width;
 }
 
 inline float areaTriangle(float base, float height) {
-  return 0.5 * base * height;
+ return 0.5 * base * height;
 }
 
 int main() {
-    float radius, side, length, width, base, height;
+      float radius, side, length, width, base, height;
 
-    cout << "Enter radius of circle: ";
-    cin >> radius;
+  cout << "Enter radius of circle: ";
+      cin >> radius;
 
-    cout << "Enter side of square: ";
-    cin >> side;
+ cout << "Enter side of square: ";
+          cin >> side;
 
-    cout << "Enter length and width of rectangle: ";
-    cin >> length >> width;
+      cout << "Enter length and width of rectangle: ";
+   cin >> length >> width;
 
     cout << "Enter base and height of triangle: ";
     cin >> base >> height;
 
-    cout << "\n--- Area Calculations ---" << endl;
+    cout << "\nArea Calculations " << endl;
     cout << "Area of Circle: " << areaCircle(radius) << endl;
     cout << "Area of Square: " << areaSquare(side) << endl;
     cout << "Area of Rectangle: " << areaRectangle(length, width) << endl;
     cout << "Area of Triangle: " << areaTriangle(base, height) << endl;
 
-    return 0;
+   
 }
