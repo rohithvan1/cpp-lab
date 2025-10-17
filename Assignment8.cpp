3877adbc8bb3906a17987cebff3b80d397099856
rohithvan1 <rohithvna1@gmail.com>
2025-10-17 11:10:35 +0530

    Create Assignment8.cpp

---
+++ Assignment8.cpp
@@ -0,0 +1,42 @@
+//8. C++ program to illustrate the use of unary operator overloading, increment ++ operator
+//overloading
+
+//Answer
+
+#include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    int count;
+
+public:
+    Counter() {
+        count = 0;
+    }
+
+    void display() {
+        cout << "Count = " << count << endl;
+    }
+
+    void operator++() {
+        count = count + 1;
+    }
+};
+
+int main() {
+    Counter c1;
+
+    cout << "Initial value: ";
+    c1.display();
+
+    cout << "After incrementing (++c1): ";
+    ++c1; // Calls overloaded operator++
+    c1.display();
+
+    cout << "Increment again (++c1): ";
+    ++c1;
+    c1.display();
+
+    return 0;
+}
